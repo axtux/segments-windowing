14fdb709f0abc2cf57b513ea1bdda6910cb037b9
Axtux <axtux@hotmail.com>
2017-04-03 17:43:58 +0200

    JCanvas is a panel displaying segments

---
+++ src/Point.java
@@ -0,0 +1,31 @@
+
+public class Point implements Comparable<Point> {
+	final int x;
+	final int y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+	/***
+	 * compare x then y
+	 */
+	public int compareTo(Point o) {
+		if(o == null) {
+			throw new NullPointerException();
+		}
+		
+		int c = Integer.compare(x, o.getX());
+		if(c != 0) {
+			return c;
+		}
+		return Integer.compare(y, o.getY());
+	}
+}
