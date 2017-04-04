b99eb1cca66f219a98639c2118641f110eb679a7
Axtux <axtux@hotmail.com>
2017-04-04 23:19:23 +0200

    can change directory

--- src/graphics/WindowSelector.java
+++ src/graphics/WindowSelector.java
@@ -1,12 +1,13 @@
 package graphics;
 
-import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -30,33 +31,29 @@ public class WindowSelector extends JFrame implements ActionListener {
 	
 	public WindowSelector(String scenesDir) {
 		super("File and Window Selector");
-		setScenesDir(scenesDir);
 		
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setLayout(new BorderLayout());
 		
 		content = new JPanel();
-		getContentPane().add(content, BorderLayout.CENTER);
+		content.setLayout(new BoxLayout(content, BoxLayout.X_AXIS));
+		add(content);
 		
 		addFileSelector();
 		addWindowSelector();
 		addButton();
 		addError();
 		// refresh when error element exists
-		refreshFileSelector();
+		setScenesDir(scenesDir);
 		// center window
-		resize();
+		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
-	}
-	
-	private void resize() {
-		pack();
 	}
 	
 	private void setScenesDir(String scenesDir) {
 		if(scenesDir == null) throw new NullPointerException();
 		this.scenesDir = scenesDir;
+		refreshFileSelector();
 	}
 	
 	private void addFileSelector() {
@@ -83,12 +80,17 @@ public class WindowSelector extends JFrame implements ActionListener {
 		for(String filename : filenames) {
 			files.addItem(filename);
 		}
-		files.revalidate();
-		files.repaint();
 		//*/
 		filesContainer.removeAll();
 		files = new JComboBox<String>(filenames.toArray(new String[0]));
 		filesContainer.add(files);
+		
+		JButton button = new JButton("Change directory");
+		button.setActionCommand("ACTION_CHANGE_DIRECTORY");
+		button.addActionListener(this);
+		filesContainer.add(button);
+		
+		pack();
 	}
 	
 	private void addWindowSelector() {
@@ -120,6 +122,9 @@ public class WindowSelector extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		System.out.println("Action performed : "+e.getActionCommand());
 		switch(e.getActionCommand()) {
+		case "ACTION_CHANGE_DIRECTORY":
+			changeDir();
+			return;
 		case "ACTION_DISPLAY_ALL":
 			displayAll();
 			return;
@@ -145,10 +150,20 @@ public class WindowSelector extends JFrame implements ActionListener {
 		new SceneWindow(scene);
 	}
 	
+	private void changeDir() {
+		JFileChooser fc = new JFileChooser(scenesDir);
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		
+		int r = fc.showOpenDialog(this);
+		if (r == JFileChooser.APPROVE_OPTION) {
+			setScenesDir(fc.getSelectedFile().getPath());
+		}
+	}
+	
 	private void error(String error) {
 		System.out.println("Error : "+error);
 		this.error.setText(error);
-		resize();
+		pack();
 	}
 	
 }
