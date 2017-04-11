280597a60231b6381888ddcdf59ad4421fe1b2b3
Axtux <axtux@hotmail.com>
2017-04-11 04:17:43 +0200

    implement transformations before passing to basicpst

--- src/data/Pst.java
+++ src/data/Pst.java
@@ -43,7 +43,59 @@ public class Pst {
 	}
 	
 	public ArrayList<Segment> getWindow(Segment window) {
-		// TODO implement
-		return null;
+		if(window.getMinX() == Integer.MIN_VALUE) {
+			// case [-∞, X']x[Y, Y']
+			return getLeftWindow(window);
+		}
+		
+		if(window.getMaxX() == Integer.MAX_VALUE) {
+			// case [X, +∞]x[Y, Y']
+			return getRightWindow(window);
+		}
+		
+		if(window.getMinY() == Integer.MIN_VALUE) {
+			// case [X, X']x[-∞, Y']
+			return getDownWindow(window);
+		}
+		
+		if(window.getMaxX() == Integer.MAX_VALUE) {
+			// case [X, X']x[Y, +∞]
+			return getUpWindow(window);
+		}
+		
+		// case [X, X']x[Y, Y']
+		return getLeftWindow(window);
+	}
+	
+	public ArrayList<Segment> getLeftWindow(Segment window) {
+		// this one is always efficient using windowing
+		return original.windowing(window.getWindow());
+	}
+	
+	public ArrayList<Segment> getRightWindow(Segment window) {
+		// oppose coordinates to be able to use efficient windowing
+		ArrayList<Segment> segments = opposed.windowing(window.oppose().getWindow());
+		for(int i = 0; i < segments.size(); ++i) {
+			segments.set(i, segments.get(i).oppose());
+		}
+		return segments;
+	}
+	
+	public ArrayList<Segment> getDownWindow(Segment window) {
+		// exchange coordinates to be able to use efficient windowing
+		ArrayList<Segment> segments = exchanged.windowing(window.exchange().getWindow());
+		for(int i = 0; i < segments.size(); ++i) {
+			segments.set(i, segments.get(i).exchange());
+		}
+		return segments;
+	}
+	
+	public ArrayList<Segment> getUpWindow(Segment window) {
+		// oppose and exchange coordinates to be able to use efficient windowing
+		ArrayList<Segment> segments = both.windowing(window.oppose().exchange().getWindow());
+		for(int i = 0; i < segments.size(); ++i) {
+			segments.set(i, segments.get(i).oppose().exchange());
+		}
+		return segments;
 	}
 }
